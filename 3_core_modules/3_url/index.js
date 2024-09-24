0631d2b55dd50097556630d24058d3fd4ca6872f
JOAO MARCOS DIAS DA MATA <70392755114@pcmt.local>
2024-09-24 12:51:20 -0400

    up

--- 3_core_modules/3_url/index.js
+++ 3_core_modules/3_url/index.js
@@ -2,9 +2,10 @@ const url = require('url');
 
 const endereco = 'https://www.google.com/catalogo?produto=cadeira';
 
-const parsedUrl = new url.URL(endereco);
+const parsedUrl = new url.URL(endereco); // permite decompor a URL
 
 console.log(parsedUrl.hostname); 
 console.log(parsedUrl.pathname);
 console.log(parsedUrl.search);
-console.log(parsedUrl.searchParams)
+console.log(parsedUrl.searchParams);
+console.log(parsedUrl.searchParams.get('produto'));
